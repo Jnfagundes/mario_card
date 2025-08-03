06638964ea103ccabbc0529ce434a479ade1cca0
José Nilton <jasfagundesmusic@gmail.com>
2025-08-03 20:38:59 -0300

    Criando as rolagens do dado

--- src/index.js
+++ src/index.js
@@ -35,6 +35,10 @@ async function getRandomBlock() {
     return result;
 }
 
+async function logRollResult(characterName,block, diceResult,attribute){
+    console.log(`${characterName} rolou um dado de ${block} ${diceResult} + ${attribute} = ${diceResult} `);
+}
+
 async function playRaceEngine(charactere1, charactere2) {
    for(let round = 1; round <= 5; round++) {
     console.log(`Rodada ${round}`);
@@ -43,29 +47,60 @@ async function playRaceEngine(charactere1, charactere2) {
     let block = await getRandomBlock();
     console.log(`Bloco sorteado: ${block}`);
 
-
-   }
-    // Jogar dados
+        // Jogar dados
 
     let diceResult1 = await rollDice();
     let diceResult2 = await rollDice();
+    
 
     //teste de habilidade
 
     let totalTesteSkill1 = 0;
     let totalTesteSkill2 = 0;
+
     if (block === "Reta") {
         totalTesteSkill1 = charactere1.VELOCIDADE + diceResult1;
         totalTesteSkill2 = charactere2.VELOCIDADE + diceResult2;
 
-        
+        await logRollResult(
+            charactere1.NOME,
+            "velocidade",
+            diceResult1,
+            charactere1.VELOCIDADE
+        );
+
+        await logRollResult(
+            charactere2.NOME,
+            "velocidade",
+            diceResult2,
+            charactere2.VELOCIDADE
+        );
+
     } else if (block === "Curva") {
         totalTesteSkill1 = charactere1.MANOBRABILIDADE + diceResult1;
         totalTesteSkill2 = charactere2.MANOBRABILIDADE + diceResult2;
+
+        await logRollResult(
+            charactere1.NOME,
+            "manobrabilidade",
+            diceResult1,
+            charactere1.MANOBRABILIDADE
+        );
+
+        await logRollResult(
+            charactere2.NOME,
+            "manobrabilidade",
+            diceResult2,
+            charactere2.MANOBRABILIDADE
+        );
+
     } else if (block === "confronto") {
-        totalTesteSkill1 = charactere1.PODER + diceResult1;
-        totalTesteSkill2 = charactere2.PODER + diceResult2;
+         let powerResult1 = charactere1.PODER + diceResult1;
+        let powerResult2 = charactere2.PODER + diceResult2;
     }
+
+   }
+
 };   
 
 (async function main() {
