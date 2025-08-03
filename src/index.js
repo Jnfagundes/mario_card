52f4c330a2bc841438aa5f336f1ff201be8690b8
José Nilton <jasfagundesmusic@gmail.com>
2025-08-03 12:44:58 -0300

    Montando a tarefa da dio

--- src/index.js
+++ src/index.js
@@ -17,9 +17,56 @@ async function rollDice() {
     return Math.floor(Math.random() *6) + 1;
 };
 
+async function getRandomBlock() {
+    let random = Math.random();
+    let result
+
+    switch (true) {
+        case random < 0.33:
+            result = "Reta"
+            break;
+        case random < 0.66:
+            result = "Curva"
+            break;
+        default:
+            result = "confronto";
+    }
+
+    return result;
+}
+
 async function playRaceEngine(charactere1, charactere2) {
-    
-};
+   for(let round = 1; round <= 5; round++) {
+    console.log(`Rodada ${round}`);
+
+    // sortear bloco
+    let block = await getRandomBlock();
+    console.log(`Bloco sorteado: ${block}`);
+
+
+   }
+    // Jogar dados
+
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
+
+    //teste de habilidade
+
+    let totalTesteSkill1 = 0;
+    let totalTesteSkill2 = 0;
+    if (block === "Reta") {
+        totalTesteSkill1 = charactere1.VELOCIDADE + diceResult1;
+        totalTesteSkill2 = charactere2.VELOCIDADE + diceResult2;
+
+        
+    } else if (block === "Curva") {
+        totalTesteSkill1 = charactere1.MANOBRABILIDADE + diceResult1;
+        totalTesteSkill2 = charactere2.MANOBRABILIDADE + diceResult2;
+    } else if (block === "confronto") {
+        totalTesteSkill1 = charactere1.PODER + diceResult1;
+        totalTesteSkill2 = charactere2.PODER + diceResult2;
+    }
+};   
 
 (async function main() {
     console.log(`Iniciando o jogo...🚗🚗. Corrida entre ${player1.NOME} e ${player2.NOME}\n`);
