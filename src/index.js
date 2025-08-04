fddd1a98dfefcae6746e30aa037ec12180fb0a33
José Nilton <jasfagundesmusic@gmail.com>
2025-08-03 21:04:11 -0300

    Verificando o vencedor

--- src/index.js
+++ src/index.js
@@ -33,11 +33,11 @@ async function getRandomBlock() {
     }
 
     return result;
-}
+};
 
 async function logRollResult(characterName,block, diceResult,attribute){
-    console.log(`${characterName} rolou um dado de ${block} ${diceResult} + ${attribute} = ${diceResult} `);
-}
+    console.log(`${characterName} rolou um dado de ${block} ${diceResult} + ${attribute} = ${diceResult + attribute} `);
+};
 
 async function playRaceEngine(charactere1, charactere2) {
    for(let round = 1; round <= 5; round++) {
@@ -97,14 +97,54 @@ async function playRaceEngine(charactere1, charactere2) {
     } else if (block === "confronto") {
          let powerResult1 = charactere1.PODER + diceResult1;
         let powerResult2 = charactere2.PODER + diceResult2;
+
+        console.log(`${charactere1.NOME} confrontou com ${charactere2.NOME}!`);
+        await logRollResult(
+            charactere1.NOME,
+            "poder",
+            diceResult1,
+            charactere1.PODER
+        );
+
+        await logRollResult(
+            charactere2.NOME,
+            "poder",
+            diceResult2,
+            charactere2.PODER
+        );
+
+        if (powerResult1 > powerResult2) {
+            if(charactere2.PONTOS > 0) {
+                charactere2.PONTOS --;
+            }
+        }
+
+         if (powerResult2 > powerResult1) {
+            if(charactere1.PONTOS > 0) {
+                charactere1.PONTOS --;
+            }
+        }
+
+        if (powerResult1 === powerResult2) {
+            console.log("Empate! Nenhum ponto foi perdido.");
+        }
+
+    //verificando o vencedor
+    if (totalTesteSkill1 > totalTesteSkill2) {
+        console.log(`${charactere1.NOME} marcou um ponto!`);
+        charactere1.PONTOS ++;
+    } else if (totalTesteSkill2 > totalTesteSkill1) {
+        console.log(`${charactere2.NOME} marcou um ponto!`);
+        charactere2.PONTOS ++;
     }
 
+    console.log('-----------------------------------');
    }
 
+}
 };   
 
 (async function main() {
     console.log(`Iniciando o jogo...🚗🚗. Corrida entre ${player1.NOME} e ${player2.NOME}\n`);
     await playRaceEngine(player1, player2);
 })();
-
